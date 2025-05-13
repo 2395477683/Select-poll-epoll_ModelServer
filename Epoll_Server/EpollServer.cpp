771d9c65d2af10ce71c11124ba7b678b97e2a291
飞萤逐火 <2395477683@qq.com>
2025-05-14 00:34:40 +0800

    Add files via upload

---
+++ Epoll_Server/EpollServer.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<unistd.h>
+#include<netdb.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+
+using namespace std;
+
+int initserver(int port);
+
+int main(int argc,char* argv[]){
+    if(argc!=2){
+        cout<<"QWQ:参数不够哦！你缺少了端口";
+        return -1;
+    }
+
+    int listensock=initserver(atoi(argv[1]));
+    cout<<"listensock="<<listensock<<endl;
+
+    int epollfd=epoll_create(1);
+
+    epoll_event ev;
+    ev.data.fd=listensock;
+    ev.events=EPOLLIN;
+
+    epoll_ctl(epollfd,EPOLL_CTL_ADD,listensock,&ev);
+
+    epoll_event evs[10];
+
+    while(true){
+
+        int infds=epoll_wait(epollfd,evs,10,-1);
+
+        if(infds<0){
+            perror("epoll() failed");break;
+        }
+
+        if(infds==0){
+            cout<<"epoll timeout"<<endl;
+            continue;
+        }
+
+        for(int i=0;i<=maxfd;++i){
+
+            if(evs[i].data.fd==listensock){
+                struct sockaddr_in client;
+                socklen_t len=sizeof(client);
+                int clientsock=accept(listensock,(struct sockaddr*)&client,&len);
+                if(clientsock<0){
+                    perror("client accept() failed");
+                    continue;
+                }
+
+                cout<<"client accept success,clientsock="<<clientsock<<endl;
+
+                ev.data.fd=clientsock;
+                ev.events=EPOLLIN;
+
+                epoll_ctl(epollfd,EPOLL_CTL_ADD,clientsock,&ev);
+
+            }
+            else{
+                char buffer[1024];
+                memset(buffer,0,sizeof(buffer));
+                if(recv(evs[i].data.fd,buffer,sizeof(buffer),0)<=0){
+                    cout<<"client(i="<<i<<"),disconnected"<<endl;
+
+                    close(evs[i].data.fd);
+                    
+                    fds[i].fd=-1;
+
+                }
+                else{
+                    cout<<"recv(i="<<evs[i].data.fd<<"):"<<buffer<<endl;
+
+                    send(evs[i].data.fd,buffer,strlen(buffer),0);
+                }
+            }
+        }
+
+    }
+
+
+}
+
+int initserver(int port){
+    int sock=socket(AF_INET,SOCK_STREAM,0);
+    if(sock<0){
+        perror("socket() failed");
+        return -1;
+    }
+
+    int opt=1;unsigned int len=sizeof(opt);                                 //为套接字启用端口地址重用功能，允许该套接字绑定到一个已经在使用中的地址和端口
+    setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&opt,len);
+
+    struct sockaddr_in servaddr;
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    servaddr.sin_port=htons(port);
+
+    if(bind(sock,(struct sockaddr*)&servaddr,sizeof(servaddr))<0){
+        perror("bind() failed");return -1;
+    }
+    
+    if(listen(sock,5)!=0){
+        perror("listen() failed");return -1;
+    }
+
+    return sock;
+}
